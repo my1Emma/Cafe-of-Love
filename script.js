dd954ed5eb2fe0d08515ced105965f3f684ded93
“my1Emma” <emmanuelaraymond5@gmail.com>
2023-05-22 10:00:12 -0400

    Experimented with the line changing function

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
+// functions //
+function lineChange(direction){
+  if (direction == "forward"){
+    i ++;
+    textArea.innerHTML = line[i]
+    
+  } else {
+    i --
+    textArea.innerHTML = line[i]
+  }
+  // If textArea[i] out of range, then one of the buttons will be hidden and not interactive // 
+  if (i < 0 || i >= line.length){
+    document.getElementById(direction).style.visibility = "hidden";
+    textArea.innerHTML = "No lines here!!"
+  } else { 
+    // If textArea[i] in range, then the buttons are visible and interactive //
+    back.style.visibility = "visible";
+    forward.style.visibility = "visible";
+  }
+}
+// Lists and Variables //
+let line = ["Ngahhh Oshi-san looks so pretty from this angle...", "I think he might just be da prettiest in da world!", "I hope that he's proud of us, of Valkyrie-- we've come so far..."];
+let i = 0
+let forward = document.getElementById("forward");
+let back = document.getElementById("back")
+let textArea = document.getElementById("script");
 
-let line = "this is a test";
+textArea.innerHTML = line[i];
 
-let textArea = document.querySelector("#script");
+// Event Listeners //
 
-textArea.textContent = line;
+forward.addEventListener('click', function(){
+  lineChange("forward");
+});
+
+back.addEventListener('click', function(){
+  lineChange("back");
+});
+
